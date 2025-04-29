5525aa8cbac0a86b6cc756e45870d2d34d963fd3
lilian <rusnaklilik07@gmail.com>
2025-04-29 10:29:06 +0300

    first commit

---
+++ src/components/DisplayNav/DisplayNav.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { assets } from '../../assets/assets';
+
+const DisplayNav = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className='w-full  flex justify-between items-center font-semibold'>
+                <div className='flex items-center gap-2'>
+                    <img onClick={() => navigate(-1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
+                    <img onClick={() => navigate(1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+                </div>
+                <div className='flex items-center gap-4'>
+                    <p className='bg-blue-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>RL</p>
+                </div>
+            </div>
+            
+        </>
+    )
+}
+
+export default DisplayNav
